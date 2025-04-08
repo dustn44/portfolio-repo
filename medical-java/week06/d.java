8111c593fc9880cd93f63fb308233a2708ad885d
dustn44 <byeonsu5@gmail.com>
2025-04-09 01:36:47 +0900

    Create d.java

---
+++ medical-java/week06/d.java
@@ -0,0 +1,13 @@
+// 2025-04-08 4번 문제
+// 1부터 20까지 합의 결과를 출력하는 프로그램을 구현(for문 사용)
+package week_6;
+
+public class d {
+
+   public static void main(String[] args) {
+      int sum = 0;
+      for(int i=0;i<=20;i=i+1)
+         sum=sum+i;
+      System.out.print(sum);
+   }
+}
