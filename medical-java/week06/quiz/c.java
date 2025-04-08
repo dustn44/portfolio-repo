069dde13c8aed6fc760db27eb9453ca420dbad7d
dustn44 <byeonsu5@gmail.com>
2025-04-09 00:57:38 +0900

    Create c.java

---
+++ medical-java/week06/quiz/c.java
@@ -0,0 +1,11 @@
+// 2025-04-08 3번 문제 
+// For문을 활용하여 자신의 이름을 8번 반복 출력하는 프로그램을 구현 *for (int a=3;   ;   )을 반드시 사용 
+package week_6;
+
+public class c {
+
+	public static void main(String[] args) {
+		for(int a=3;a<=10;a++)
+		System.out.println("박연수");
+  }
+}
